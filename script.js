811a17181d7c846a16a47656b64c1431f4bb7e5f
the-HarshUpadhyay <harsh.upadhyay.mit@gmail.com>
2026-06-10 17:03:44 +0530

    added getHumanChoice() for user choice

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-console.log("Hello World!");
+//console.log("Hello World!");
 function getComputerChoice(){
     let choice = ["rock","paper","scissor"]
     return choice[Math.floor(Math.random()*3)];
 }
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let choice = prompt('Enter Choice');
+    switch (choice){
+        case "rock":
+        case "paper":
+        case "scissor":
+            return choice;
+            break;
+        default:
+            alert("Enter a valid choice!!");
+            break;
+    }
+}
+//console.log(getComputerChoice());
+//console.log(getHumanChoice());
