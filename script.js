3fcb89cd89bb567f7dbadb6b83c4aa92e372d26c
the-HarshUpadhyay <harsh.upadhyay.mit@gmail.com>
2026-06-10 16:59:51 +0530

    added getComputerChoice() for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Hello World!");
+function getComputerChoice(){
+    let choice = ["rock","paper","scissor"]
+    return choice[Math.floor(Math.random()*3)];
+}
+console.log(getComputerChoice());
